ab997213dbfd17d9f238ea1663c80eeef3d3582d
Cr1mson48 <kachanov.andrii@yandex.ru>
2022-04-05 21:01:02 +0300

    1

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from flask_login import LoginManager
+from forms.users import RegisterForm
+from data.__all_models import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sBPkvAZXVsSNPq1SAjNQ'
+
+
+def main():
+    db_session.global_init("db/data.db")
+    app.run()
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+if __name__ == "__main__":
+    main()
